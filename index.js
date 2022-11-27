861e4a51347533170a86095dcbac1d9f7a616395
antonyosebe <osebe.anto@gmail.com>
2022-11-27 17:56:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const writeCards = (array, surprise) => {
+    let emptyArray = []
+    let name = "";
+
+    for (name of array) {
+        emptyArray.push(`Thank you, ${name}, for the wonderful ${surprise} gift!`)
+    }
+    return emptyArray
+
+}
+
+const countDown = (number) => {
+    for (let i = number; i >= 0; i--) {
+        console.log(i)
+    }
+}
